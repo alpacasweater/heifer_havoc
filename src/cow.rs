6d1cde5629939d7cd4bfcfbcdb05ac64eb6dddde
Ben <babiggs@vt.edu>
2025-05-10 21:03:14 -0400

    Added mobile fire button.

--- src/cow.rs
+++ src/cow.rs
@@ -1,6 +1,7 @@
 
-use bevy::prelude::*;
+// use bevy::prelude::*;
 use avian3d::prelude::*;
+use bevy::{color::palettes::basic::*, prelude::*, winit::WinitSettings};
 use std::f32::consts::PI;
 
 use crate::asset_loader::SceneAssets;
@@ -31,13 +32,95 @@ pub struct Poop;
 pub struct CowPlugin;
 impl Plugin for CowPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(VirtualJoystickPlugin::<String>::default())
-        .add_systems(PostStartup, (spawn_cow, spawn_joystick))
+        app
+        .insert_resource(WinitSettings::desktop_app())
+        .add_plugins(VirtualJoystickPlugin::<String>::default())
+        .add_systems(PostStartup, (spawn_cow, spawn_joystick, button_setup))
         .add_systems(
             Update, 
-            (cow_movement_controls, cow_weapon_controls, cow_shield_controls).chain().in_set(InGameSet::UserInput)
+            (button_system, cow_movement_controls, cow_weapon_controls, cow_shield_controls).chain().in_set(InGameSet::UserInput)
         );
     }
+}
+
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &mut BorderColor,
+            &Children,
+        ),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                **text = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = RED.into();
+            }
+            Interaction::Hovered => {
+                **text = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                **text = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
+
+fn button_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn((
+                    Button,
+                    Node {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // // horizontally center child text
+                        // justify_content: JustifyContent::Center,
+                        // // vertically center child text
+                        // align_items: AlignItems::Center,
+                        position_type: PositionType::Absolute,
+                        left: Val::Percent(80.),
+                        bottom: Val::Percent(50.),
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BorderRadius::MAX,
+                    BackgroundColor(NORMAL_BUTTON),
+                ))
+                .with_child((
+                    Text::new("Button"),
+                    TextFont {
+                        font_size: 33.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+        });
 }
 
 fn spawn_joystick(mut cmd: Commands, asset_server: Res<AssetServer>) {
@@ -57,7 +140,7 @@ fn spawn_joystick(mut cmd: Commands, asset_server: Res<AssetServer>) {
             width: Val::Px(150.),
             height: Val::Px(150.),
             position_type: PositionType::Absolute,
-            left: Val::Percent(50.),
+            left: Val::Percent(10.),
             bottom: Val::Percent(15.),
             ..default()
         },
@@ -143,6 +226,7 @@ fn cow_movement_controls(
 fn cow_weapon_controls(
     mut commands: Commands,
     query: Query<&mut Transform, With<Cow>>, 
+    mut interaction_query: Query<&Interaction, With<Button>>,
     keyboard_input: Res<ButtonInput<KeyCode>>, 
     scene_assets: Res<SceneAssets>,
 ) {
@@ -150,8 +234,22 @@ fn cow_weapon_controls(
         return;
     };
 
-    // pitch
-    if keyboard_input.pressed(KeyCode::Space) {
+    let mut mobile_button_pressed = false;
+    for interaction in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                mobile_button_pressed = true;
+            }
+            Interaction::Hovered => {
+                mobile_button_pressed = false;
+            }
+            Interaction::None => {
+                mobile_button_pressed = false;
+            }
+        }
+    }
+
+    if keyboard_input.pressed(KeyCode::Space) || mobile_button_pressed {
         commands.spawn((MovingObjectBundle{
             velocity: Velocity::new(transform.forward() * COW_PROJECTILE_SPEED), // Adjust speed as needed
             acceleration: Acceleration::new(Vec3::ZERO),
