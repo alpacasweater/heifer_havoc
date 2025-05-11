952e7c7dd8e27a29169e551823f1982c6334f690
Ben <babiggs@vt.edu>
2025-05-10 21:40:25 -0400

    Trying to fix the mobile window size.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod schedule;
 
 use avian3d::prelude::*;
 use bevy::prelude::*;
+use bevy::window::{PresentMode, WindowTheme};
 // use debug::DebugPlugin;
 use movement::MovementPlugin;
 use cow::CowPlugin;
@@ -21,13 +22,33 @@ use collisions::CollisionsPlugin;
 
 fn main() {
     App::new()
-    .insert_resource(ClearColor(Color::srgb(0.1, 0.4, 0.1)))
-    .insert_resource(AmbientLight {
-        color: Color::default(),
-        brightness: 0.0
-    })
     // Bevy built-ins
-    .add_plugins(DefaultPlugins)
+    // .add_plugins(DefaultPlugins)
+    .add_plugins((
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Heifer Havoc".into(),
+                name: Some("bevy.app".into()),
+                // resolution: (500., 300.).into(),
+                present_mode: PresentMode::AutoVsync,
+                // Tells Wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells Wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                window_theme: Some(WindowTheme::Dark),
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: true,
+                    ..Default::default()
+                },
+                // This will spawn an invisible window
+                // The window will be made visible in the make_visible() system after 3 frames.
+                // This is useful when you want to avoid the white window that shows up before the GPU is ready to render the app.
+                visible: true,
+                ..default()
+            }),
+            ..default()
+        }),
+    ))
     // Avian3d physics engine
     .add_plugins(PhysicsPlugins::default())
     // User Defined Plugins
